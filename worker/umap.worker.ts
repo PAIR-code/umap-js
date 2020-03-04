26e8ec65e1b16d9ddc18b8cb4ad8ffed2f65bd00
Jana Beck <jana.beck@stitchfix.com>
2020-03-04 01:32:21 -0800

    working proof-of-concept build + sketch of UMAPWorkerWrapper

--- worker/umap.worker.ts
+++ worker/umap.worker.ts
@@ -1,5 +1,25 @@
 import { UMAP } from '../src/lib';
 
-self.onmessage = ({ data }) => {
-  console.log(data);
-};
+async function messageHandler({ data: { id, method, args } }) {
+  console.log(
+    `UMAPWorker: received instruction to execute 'umap.${method}' with ${args.length} args`
+  );
+  switch (method) {
+    case 'fit': {
+      const umap = new UMAP();
+      const embedding = await umap.fit(args[0]);
+      // @ts-ignore
+      postMessage({ id, method, results: { embedding } });
+      break;
+    }
+    default: {
+      // @ts-ignore
+      postMessage({
+        id,
+        error: `Unknown method requested of UMAPWorker: 'umap.${method}'`,
+      });
+    }
+  }
+}
+
+onmessage = messageHandler;
